6af4e98fc7f902c7eefc929c6c902e35c522267a
AndrewMcBurney <andrewrobertmcburney@gmail.com>
2018-01-23 17:21:51 -0500

    Implemented different way of persisting data for linkage cache using pstore.

--- Library/Homebrew/os/mac/linkage_store.rb
+++ Library/Homebrew/os/mac/linkage_store.rb
@@ -1,5 +1,6 @@
 #
-# LinkageStore is a class which acts as an interface to SQLite3
+# LinkageStore is a class which acts as an interface to a persistent storage
+# mechanism
 #
 # If the cache hasn't changed, don't do extra processing in LinkageChecker.
 # Instead, just fetch the data stored in the cache
@@ -8,111 +9,75 @@
 require "os/mac/linkage_database"
 
 class LinkageStore
-  # Updates cached values in SQLite3 tables according to the type of data stored
+  include LinkageDatabase
+
+  attr_reader :key
+
+  # Initializes new LinkageStore class
   #
-  # @param  [String] key
-  # @param  [Hash] array_linkage_values
-  # @param  [Hash] hash_linkage_values
+  # @param  [String] keg_name
   # @return [nil]
-  def update!(key:,
-    array_linkage_values: {
+  def initialize(keg_name)
+    @key = keg_name
+  end
+
+  # Updates cached values in pstore according to the type of data stored
+  #
+  # @param  [Hash] path_values
+  # @param  [Hash] hash_values
+  # @return [nil]
+  def update!(
+    path_values: {
       system_dylibs: %w[], variable_dylibs: %w[], broken_dylibs: %w[],
       undeclared_deps: %w[], unnecessary_deps: %w[]
     },
-    hash_linkage_values: {
+    hash_values: {
       brewed_dylibs: {}, reverse_links: {}
-    })
-    array_linkage_values.each do |type, table_values|
-      insert_path_values(type, key, table_values)
-    end
-
-    hash_linkage_values.each do |type, values|
-      values.each do |label, list|
-        insert_hash_values(type, key, list, label)
-      end
+    }
+  )
+    db.transaction do
+      db[key] = {
+        path_values: path_values,
+        brewed_dylibs: format_hash_values(hash_values[:brewed_dylibs]),
+        reverse_links: format_hash_values(hash_values[:reverse_links]),
+      }
     end
   end
 
   # Fetches a subset of paths by looking up an array of keys
   #
   # @param  [String] type
-  # @param  [Array[String]] keys
   # @return [Array[String]]
-  def fetch_path_values!(type:, keys:)
-    LinkageDatabase.db.execute(
-      <<~SQL
-        SELECT path FROM linkage
-          WHERE type = '#{type}'
-          AND name IN(#{LinkageDatabase.format_database_list(keys)});
-      SQL
-    ).flatten
+  def fetch_path_values!(type:)
+    db.transaction { db[key][:path_values][type.to_sym] }
   end
 
   # Fetches path and label values from a table name given an array of keys and
   # returns them in a hash format with the labels as keys to an array of paths
   #
   # @param  [String] type
-  # @param  [Array[String]] keys
   # @return [Hash]
-  def fetch_hash_values!(type:, keys:)
-    hash = {}
-    LinkageDatabase.db.execute(
-      <<~SQL
-        SELECT label, path FROM linkage
-          WHERE type = '#{type}'
-          AND name IN(#{LinkageDatabase.format_database_list(keys)});
-      SQL
-    ).each { |row| (hash[row[0]] ||=[]) << row[1] }
-    hash
+  def fetch_hash_values!(type:)
+    db.transaction { db[key][type.to_sym] }
   end
 
-  # Deletes rows given an array of keys. If the row's name
-  # attribute contains a value in the array of keys, then that row is deleted
+  # Flushes the cache for the given 'key' name
   #
-  # @param  [Array[String]] keys
   # @return [nil]
-  def flush_cache_for_keys!(keys:)
-    LinkageDatabase.db.execute(
-      <<~SQL
-        DELETE FROM linkage
-          WHERE name IN(#{LinkageDatabase.format_database_list(keys)});
-      SQL
-    )
+  def flush_cache!
+    db.transaction { db.delete(key) }
   end
 
   private
 
-  def insert_path_values(type, key, values)
-    return if values.empty?
-
-    LinkageDatabase.db.execute(
-      <<~SQL
-        INSERT INTO linkage (name, path, type)
-          VALUES #{format_array_database_values(key, values, type)};
-      SQL
-    )
-  end
-
-  def insert_hash_values(type, key, values, label)
-    return if values.empty?
-
-    LinkageDatabase.db.execute(
-      <<~SQL
-        INSERT INTO linkage (name, path, type, label)
-          VALUES #{format_hash_database_values(key, values, type, label)};
-      SQL
-    )
-  end
-
-  def format_array_database_values(key, values, type)
-    values
-      .map { |value| "('#{key}', '#{value}', '#{type}')" }
-      .join(", ")
-  end
-
-  def format_hash_database_values(key, values, type, label)
-    values
-      .map { |value| "('#{key}', '#{value}', '#{type}', '#{label}')" }
-      .join(", ")
+  # `pstore` throws an error if the hash is empty, or if the hash contains Set
+  # values
+  #
+  # Error: can't dump hash with default proc
+  #
+  # @param  [Hash] hash
+  # @return [Hash]
+  def format_hash_values(hash)
+    hash.empty? ? nil : hash.map { |k, v| { k => v.to_a } }
   end
 end
